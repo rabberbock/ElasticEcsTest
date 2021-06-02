67e2b946393ee264ca09b1b818203ecb5dbe078f
Raffi Abberbock <rabberbock@gmail.com>
2021-06-02 15:41:47 -0400

    initial commit

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Elastic.CommonSchema.Serilog;
+using Serilog;
+
+namespace SampleEcs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var loggerWithEcs = new LoggerConfiguration()
+                .WriteTo.Console(new EcsTextFormatter())
+                .CreateLogger();
+            loggerWithEcs.Information("Hello World With ECS");
+
+            var loggerWithoutEcs = new LoggerConfiguration()
+                .WriteTo.Console()
+                .CreateLogger();
+            loggerWithoutEcs.Information("Hello World Without ECS");
+       }
+    }
+}
